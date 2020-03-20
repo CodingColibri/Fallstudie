b43e4bee93484be606703e76c53f6d486618d37b
Linda <linda.theresia@live.de>
2020-03-20 19:02:53 +0100

    Ordner und Basis Klassen ergänzt

---
+++ Backend/Fallstudie/src/test/java/Backend/Fallstudie/FallstudieApplicationTests.java
@@ -0,0 +1,13 @@
+package Backend.Fallstudie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FallstudieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
