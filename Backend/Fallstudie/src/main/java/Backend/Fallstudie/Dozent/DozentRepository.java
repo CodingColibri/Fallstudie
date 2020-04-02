63e587596ab6a099ba91b2fecd7ea29ad7bbb5fa
Linda <linda.theresia@live.de>
2020-04-02 22:24:16 +0200

    SpringBoot Basis entfernt

--- Backend/Fallstudie/src/main/java/Backend/Fallstudie/Dozent/DozentRepository.java
+++
@@ -1,9 +0,0 @@
-package Backend.Fallstudie.Dozent;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface DozentRepository extends JpaRepository<Dozent, Integer> {
-    ;
-}
--- Backend/Fallstudie/src/main/java/Backend/Fallstudie/Modul/ModulRepository.java
+++
@@ -1,9 +0,0 @@
-package Backend.Fallstudie.Modul;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ModulRepository extends JpaRepository<Modul, Integer> {
-    ;
-}
